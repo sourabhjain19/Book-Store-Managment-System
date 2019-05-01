db5b273493211c6cb566dadf7cc993c15d1f5e4c
Sudeep G <sudeepgumaste@github.com>
2019-05-01 17:07:30 +0530

    deleted unnecessary files

--- commands.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO items VALUES('scale',5.0,'stationary','apsara',NULL,NULL,10);
-INSERT INTO items VALUES('pencil',3.0,'stationary','natraj',NULL,NULL,10);
-INSERT INTO items VALUES('sharpner',5.0,'stationary','natraj',NULL,NULL,10);
-INSERT INTO items VALUES('pen',10.0,'stationary','cello',NULL,NULL,10);
-INSERT INTO items VALUES('Navbharat',3.0,'newspaper',NULL,'navbharat',NULL,10);
-INSERT INTO items VALUES('ComputerScience',350.0,'book',NULL,NULL,'Gundurao',4);
